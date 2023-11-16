a25b809d9a7c3d54fecd24c289014833ab69b2ac
DESKTOP-NBCPEOS\narkk <narkkarux.t@gmail.com>
2023-11-16 10:00:32 +0700

    Nui | Commit env setup files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles.css";
+
+document.body.innerHTML = '<div id="app"></div>';
+
+const root = createRoot(document.getElementById("app"));
+root.render(<App />);
