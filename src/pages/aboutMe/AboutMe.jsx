df9398e2e27bf73af1746852ed8f61c899e05107
Narkkarux Tripiyaratana <narkkarux.tri@pantavanij.com>
2024-01-12 22:53:01 +0700

    Nui | Improve hero and section style

--- src/pages/aboutMe/AboutMe.jsx
+++ src/pages/aboutMe/AboutMe.jsx
@@ -1,54 +1,81 @@
+import ArrowDown from "../../assets/arrow-down.png";
+
 function AboutMe() {
   return (
     <>
       <section className="hero">
-        <img src="https://assets.api.uizard.io/api/cdn/stream/d2d3bae8-33a0-4ebe-9a4f-6a161259b068.png" className="hero-bg" alt="a rainbow" />
-        <h1 className="hero-title">Hi, this is Pattaraporn!</h1>
-      </section>
-
-      <section className="section pt-0">
-        <div className="section-wrapper">
-          <h1 className="section-title">About me</h1>
-          <p className="section-text">I'm a language enthusiast turned professional Thai (th-TH) translator with a degree in linguistics and a decade of experience in translation and localization plus project management.</p>
-          <p>I turn words into bridges, crafting precise and culturally attuned localization to create impactful connections. Glad to see you here!</p>
-          <button className="btn btn-primary active section-btn center">Connect on Linkedin</button>
+        <h1 className="hero-title">Hi hi 👋🏻 this is Pattaraporn!</h1>
+        <img
+          src="https://placekitten.com/240/240"
+          className="hero-profile-img"
+          alt="profile"
+        />
+        <p className="hero-sub">
+          I turn words into bridges, crafting precise and culturally attuned{" "}
+          <br />
+          <span className="hero-sub-strong">localization</span> to create
+          impactful connections <br />
+          and smooth <span className="hero-sub-strong">user experience</span>
+          🌈✨
+        </p>
+        <div className="icon-divider">
+          <img className="icon" src={ArrowDown} alt="next section arrow down" />
         </div>
-        <img src="https://assets.api.uizard.io/api/cdn/stream/d2f3cbc3-267e-469b-b9ef-264b46310cb9.png" alt="kid sunset" className="section-side-img" />
-      </section>
-
-      <section className="section reverse">
-        <div className="section-wrapper">
-          <h1 className="section-title">Translation and localization</h1>
-          <p className="section-text">I've engaged in a wide spectrum of projects, covering academic materials, interviews, surveys, and video subtitles. My expertise revolves around software localization for mobile apps and websites. In this specialized field, I've established style guides, glossaries and other language assets to ensure top-quality localization. I also provide localization audit and consultation to my client.</p>
-          <button className="section-btn btn btn-outline-light">See sample projects</button>
+        <div className="text-center">
+          <h2>My Expertise</h2>
         </div>
-        <img src="https://assets.api.uizard.io/api/cdn/stream/cc850735-1065-4b81-8c7a-fd664d4ca9d8.png" alt="worker" className="section-side-img" />
-      </section>
-
-      <section className="section">
-        <div className="section-wrapper">
-          <h1 className="section-title">UX writing</h1>
-          <p className="section-text">Collaborating within a vibrant UX team comprising content designers, product designers, researchers, project managers, engineers, and many more, I contribute to crafting content that enhances the user experience. I help ensure that the content design meets usability, accessibility, and user-centric standards, while also being localization-friendly so the brand is ready to go local and global.</p>
-          <button className="section-btn btn btn-primary ">See sample projects</button>
-        </div>
-        <img src="https://assets.api.uizard.io/api/cdn/stream/4fce335d-594d-40b7-89a3-2504be841f71.png" alt="people around the world" className="section-side-img" />
-      </section>
-
-      <section className="section reverse">
-        <div className="section-wrapper">
-          <h1 className="section-title">Blog writing</h1>
-          <p className="section-text">I share my Korean travel and language-learning experiences through blog articles giving the readers a glimpse into the culture, language and adventures of Korea.</p>
-          <button className="section-btn btn btn-outline-light ">Visit website</button>
-        </div>
-        <img src="https://assets.api.uizard.io/api/cdn/stream/f1288968-921f-4ccb-b628-d3e9de467710.png" alt="blog-writing" className="section-side-img" />
       </section>
 
       <section className="section">
-        <div className="section-wrapper">
-          <h1 className="section-title">Teaching Thai language</h1>
-          <p className="section-text">I offer personalized lessons to help Thai language learners achieve their goals. My students come from italy, Myanmar, Korea, China and Singapore.</p>
+        <div className="section-title">
+          <h4>
+            Translation &<br /> localization
+          </h4>
         </div>
-        <img src="https://assets.api.uizard.io/api/cdn/stream/f1288968-921f-4ccb-b628-d3e9de467710.png" alt="people around the world" className="section-side-img" />
+        <p className="section-text body-2">
+          I've engaged in a wide spectrum of projects, covering academic
+          materials, interviews, surveys, and video subtitles. My expertise
+          revolves around software localization for mobile apps and websites. In
+          this specialized field, I've established style guides, glossaries and
+          other language assets to ensure top-quality localization. I also
+          provide localization audit and consultation to my client.
+        </p>
+      </section>
+
+      <section className="section section-revert">
+        <div className="section-title">
+          <h4>UX writing</h4>
+        </div>
+        <p className="section-text body-2">
+          Collaborating within a vibrant UX team comprising content designers,
+          product designers, researchers, project managers, engineers, and many
+          more, I contribute to crafting content that enhances the user
+          experience. I help ensure that the content design meets usability,
+          accessibility, and user-centric standards, while also being
+          localization-friendly so the brand is ready to go local and global.
+        </p>
+      </section>
+
+      <section className="section">
+        <div className="section-title">
+          <h4>Blog writing</h4>
+        </div>
+        <p className="section-text body-2">
+          I share my Korean travel and language-learning experiences through
+          blog articles giving the readers a glimpse into the culture, language
+          and adventures of Korea.
+        </p>
+      </section>
+
+      <section className="section section-revert">
+        <div className="section-title">
+          <h4>Thai for Foreigners</h4>
+        </div>
+        <p className="section-text body-2">
+          I offer personalized lessons to help Thai language learners achieve
+          their goals. My students come from italy, Myanmar, Korea, China and
+          Singapore.
+        </p>
       </section>
     </>
   );
