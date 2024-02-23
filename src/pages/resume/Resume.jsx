54c2fbd924184545dc043def18a9b323dcb63e11
Narkkarux Tripiyaratana <narkkarux.tri@pantavanij.com>
2024-02-23 10:58:56 +0700

    Nui | Implement resume page

--- src/pages/resume/Resume.jsx
+++ src/pages/resume/Resume.jsx
@@ -1,18 +1,13 @@
 function Resume() {
+  const style = { width: "100%", height: "100vh" };
+
   return (
     <>
-      <img src="https://assets.api.uizard.io/api/cdn/stream/f6ccfb77-809f-4f7d-aada-72e3891214e4.jpg" alt="brick wall" className="portfolio-header-img" />
-      <div className="portfolio">
-        <div className="card">
-          <div className="header">
-            <button className="btn btn-outline-light">Download PDF</button>
-            <h1 className="header-title">Selected Projects</h1>
-            <div className="header-sub-title">Say something about the resume klgjgjergjdlg</div>
-            <hr className="divider" />
-          </div>
-          <div className="project-list"></div>
-        </div>
-      </div>
+      <iframe
+        src="https://docs.google.com/gview?url=https://github.com/mookmook-71/portfolio/raw/dev/src/assets/(Resume)%20Pattaraporn%20Tripiyaratana.pdf&embedded=true"
+        style={style}
+        frameborder="0"
+      ></iframe>
     </>
   );
 }
